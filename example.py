00785c90875b0bc4dac1967164b850a29168e916
Behrouz.K <behrouz@shery.tech>
2020-06-10 12:32:26 +0430

    update example

--- example.py
+++ example.py
@@ -1,11 +1,17 @@
-from configs import Configs
+from configs import Configs    
 
-print(Configs["A"])
+print(Configs["optional_env_test1"])
+# >>> 1
 
-Configs["B"] = "K"
+# Set configs in runtime
+Configs["runtime_config"] = "hello"
+print(Configs["runtime_config"])
+# >>> hello
 
-print(Configs["B"])
+# Change a config in runtime
+Configs["runtime_config"] = "bye"
+print(Configs["runtime_config"])
+# >>> bye
 
-del Configs["B"]
-
-print(Configs["B"])Î
+# Delete a config
+del Configs["runtime_config"]
